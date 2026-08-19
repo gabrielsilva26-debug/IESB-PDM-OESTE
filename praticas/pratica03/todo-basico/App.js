a85ac87bf4ca329e09e4934ea324e73e38031ad3
unknown <24214290041@ACADEMICO.IESB.BR>
2026-08-18 21:33:29 -0300

    Atividade 4

--- praticas/pratica03/todo-basico/App.js
+++ praticas/pratica03/todo-basico/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,TextInput } from 'react-native';
 import { titulo } from './util';
 import titulo_padrao from './util';
+import {
+rotulo_input_meta, 
+rotulo_btn_cadastro_meta,
+rotulo_lista_metas
+} from './mensagens';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>{titulo}</Text>
-      <Text>{titulo_padrao}</Text>
+      <Text style={{margin: 20}}>{titulo_padrao}</Text>
+      <Text style={styles.Text}>AAA</Text>
+      <Button title='Adicionar' />
+      <Text>{rotulo_lista_metas}</Text>
+      <Button title={rotulo_btn_cadastro_meta} />
+      <TextInput style={styles.primari1} placeholder={rotulo_input_meta}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +28,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  Text:{
+    margin: 20,
+  },
+  primari1:{
+    margin: 30,
+    borderWidth: 2,
+    borderColor: '#1f2472',
+    backgroundColor: 'rgb(205, 205, 233)'
+  }
+}); 
